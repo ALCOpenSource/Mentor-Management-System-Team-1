7cca0f200dad3d46f07619dec6809d73cc236b58
Udazi Ephraim Oseiwe <udazi.osebest@gmail.com>
2023-04-04 17:15:20 +0100

    Initiated all starter files and tools needed for Frontend

---
+++ frontend/mms-Admin/Admin/src/main.ts
@@ -0,0 +1,26 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+import 'virtual:uno.css'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
