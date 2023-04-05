a3e7c757e88729a04fdf6b82ba1429e540ad19be
Udazi Ephraim Oseiwe <udazi.osebest@gmail.com>
2023-04-05 11:16:29 +0100

    Added other Helper tools for Styling and Validation

--- frontend/mms-Admin/Admin/src/main.ts
+++ frontend/mms-Admin/Admin/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueFormulate from '@braid/vue-formulate'
 
 import App from './App.vue'
 import router from './router'
@@ -19,6 +20,7 @@ const vuetify = createVuetify({
 
 const app = createApp(App)
 
+app.use(VueFormulate)
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
