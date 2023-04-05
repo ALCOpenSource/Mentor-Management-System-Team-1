de0a8f82f8bf3a0aa1db46f6cf872f8646fb2c60
Udazi Ephraim Oseiwe <udazi.osebest@gmail.com>
2023-04-05 20:09:25 +0100

    Re-structured the Starter Files for the frontend

---
+++ frontend/mms-Admin/src/layouts/authLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="text-2xl text-center font-semibold my-20">Welcome to the Auth Layout</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/mms-Admin/src/router/index.ts
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Dashboard from '../views/Dashboard/Dashboard.vue'
+import dashboardLayoutVue from '@/layouts/dashboardLayout.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'dashboardLayout',
+      component: dashboardLayoutVue,
+      children: [
+        {
+          path: '/',
+          name: 'dashboard',
+          component: Dashboard
+        }
+      ]
+    },
+    {
+      path: '/login',
+      name: 'loginLayout',
+      component: () => import('@/layouts/authLayout.vue'),
+      children: [
+        {
+          path: '/login',
+          name: 'login',
+          component: () => import('@/views/Auth/Login.vue')
+        }
+      ]
+    }
+  ]
+})
+
+export default router
