d605f23a5b4f4ccaf6a8a038bce1bd9656aa96f5
Udazi Ephraim Oseiwe <udazi.osebest@gmail.com>
2023-04-06 14:46:31 +0100

    Built the Splash screen and Login page (UI)

---
+++ frontend/mms-Admin/src/components/Auth/Splash.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="w-full h-full flex flex-col items-center justify-center splash">
+    <img src="../../assets/images/Logo-Onley-3-01 1.png" alt="logo">
+    <h1>Mentor Management System</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.splash {
+  background-color: #058B94;
+
+  h1 {
+    color: #fff;
+    font-size: 32px;
+    font-weight: 700;
+    margin-top: 2rem;
+  }
+}
+</style>
