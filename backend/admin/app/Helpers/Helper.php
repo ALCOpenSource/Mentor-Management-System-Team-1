723040b1099077b689db797f8ab3bfce8fa82cd7
Ghostscypher <bngetich69@gmail.com>
2023-04-19 17:29:06 +0300

    Fix: token issue

--- backend/admin/app/Helpers/Helper.php
+++ backend/admin/app/Helpers/Helper.php
@@ -54,6 +54,11 @@ function callStaticMethod(object $class, string $methodname, ...$args)
 function addUserSession($request)
 {
     $user = $request->user();
+
+    if (! $user->currentAccessToken() || ! $user->currentAccessToken()->token) {
+        return;
+    }
+
     $token = hash('md5', $user->currentAccessToken()->token);
     $session = $user->userSessions()->where('id', $token)->first();
 
