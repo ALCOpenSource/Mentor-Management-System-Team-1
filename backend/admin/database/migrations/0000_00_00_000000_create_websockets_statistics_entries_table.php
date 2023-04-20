55bbd60c4f52ef981ab417df3eb67bccbc65fdfd
Ghostscypher <bngetich69@gmail.com>
2023-04-20 12:29:25 +0300

    Fix: codacy issues

--- backend/admin/database/migrations/0000_00_00_000000_create_websockets_statistics_entries_table.php
+++ backend/admin/database/migrations/0000_00_00_000000_create_websockets_statistics_entries_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWebSocketsStatisticsEntriesTable extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('websockets_statistics_entries', function (Blueprint $table) {
             $table->increments('id');
@@ -28,8 +27,9 @@ class CreateWebSocketsStatisticsEntriesTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('websockets_statistics_entries');
     }
-}
+
+};
