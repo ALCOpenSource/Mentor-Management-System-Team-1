d72c63fa0e0820690229663ebb04f63eca8693f2
Bilal <ot@Otienos-MacBook-Air.local>
2023-05-07 20:28:19 +0300

    integrate faq

--- frontend/mms-Admin/src/views/Settings/FAQ.vue
+++ frontend/mms-Admin/src/views/Settings/FAQ.vue
@@ -1,49 +1,54 @@
 <template>
   <div>
-    <h1 class="text-xl font-semibold mb-5">General FAQ</h1>
-    <v-expansion-panels multiple v-model="expanded1">
-      <v-expansion-panel v-for="item in 5" :key="item">
-        <v-expansion-panel-title>
-          <IconCircleAdd v-if="!expanded1.includes(item - 1)" />
-          <IconCircleMinus v-else />
-          <p class="font-semibold ml-6">General Frequently Asked Question?</p>
-        </v-expansion-panel-title>
-        <v-expansion-panel-text> Some content </v-expansion-panel-text>
-      </v-expansion-panel>
-    </v-expansion-panels>
-    <h1 class="text-xl font-semibold my-5">Technical FAQ</h1>
-    <v-expansion-panels multiple v-model="expanded2">
-      <v-expansion-panel v-for="item in 5" :key="item">
-        <v-expansion-panel-title>
-          <IconCircleAdd v-if="!expanded2.includes(item - 1)" />
-          <IconCircleMinus v-else />
-          <p class="font-semibold ml-6">Technical Frequently Asked Question?</p>
-        </v-expansion-panel-title>
-        <v-expansion-panel-text> Some content </v-expansion-panel-text>
-      </v-expansion-panel>
-    </v-expansion-panels>
-
-    <!-- <v-expansion-panel>
-        <v-expansion-panel-title>
-          <template v-slot:actions="{ expanded }">
-            <v-icon><GitHub /></v-icon>
-          </template>
-          <h1>Heelo</h1>
-          <p class="mx-4">djjd</p>
-        </v-expansion-panel-title>
-        <v-expansion-panel-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </v-expansion-panel-text>
-      </v-expansion-panel> -->
+    <div v-if="generalFaqs?.length">
+      <h1 class="text-xl font-semibold mb-5">General FAQ</h1>
+      <v-expansion-panels multiple v-model="expanded1">
+        <v-expansion-panel v-for="(item, index) in generalFaqs" :key="item?.id">
+          <v-expansion-panel-title>
+            <IconCircleAdd v-if="!expanded1.includes(index)" />
+            <IconCircleMinus v-else />
+            <p class="font-semibold ml-6">{{ item.question }}</p>
+          </v-expansion-panel-title>
+          <v-expansion-panel-text> {{ item.answer }} </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
+    <div v-if="technicalFaqs?.length">
+      <h1 class="text-xl font-semibold my-5">Technical FAQ</h1>
+      <v-expansion-panels multiple v-model="expanded2">
+        <v-expansion-panel
+          v-for="(item, index) in technicalFaqs"
+          :key="item?.id"
+        >
+          <v-expansion-panel-title>
+            <IconCircleAdd v-if="!expanded2.includes(index)" />
+            <IconCircleMinus v-else />
+            <p class="font-semibold ml-6">{{ item.question }}</p>
+          </v-expansion-panel-title>
+          <v-expansion-panel-text> {{ item.answer }} </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { IconCircleAdd, IconCircleMinus } from "@/assets/icons";
+import { useFaqStore } from "@/store/settings/faq";
+import { storeToRefs } from "pinia";
+
+interface FaqProp {
+  id?: number;
+  question: string;
+  answer: string;
+  category: string;
+}
+
+const { fetchFaqs } = useFaqStore();
+
+fetchFaqs();
+const { generalFaqs, technicalFaqs } = storeToRefs(useFaqStore());
 
 const expanded1 = ref<Number[]>([]);
 const expanded2 = ref<Number[]>([]);
