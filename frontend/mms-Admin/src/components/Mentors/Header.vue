caaa75dc71330842c02b8096fcc304a47333700e
Udazi Ephraim Oseiwe <udazi.osebest@gmail.com>
2023-05-07 19:58:09 +0100

    Built the UI for the Mentor screens

---
+++ frontend/mms-Admin/src/components/Mentors/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex justify-between items-center">
+        <div class="flex items-center">
+          <v-avatar size="90px">
+            <v-img
+              src="https://www.caribbeangamezone.com/wp-content/uploads/2018/03/avatar-placeholder.png"
+              alt="John"
+            ></v-img>
+          </v-avatar>
+          <div class="ml-6">
+            <div class="flex items-center">
+              <h1 class="mr-2 text-xl font-semibold">Allison Davies</h1>
+              <NigerianFlag />
+            </div>
+            <p class="text-[#808080] flex gap-2 items-center">
+              Mentor
+              <span>
+                <Edit class="cursor-pointer" />
+              </span>
+            </p>
+          </div>
+        </div>
+        <div class="flex gap-4">
+          <!-- Handle routing to specified chat -->
+          <router-link to="/admin/messages">
+            <PrimaryBtn title="Send Message" />
+          </router-link>
+          <router-link to="/admin/mentors">
+            <SecondaryBtn title="CLose" />
+          </router-link>
+        </div>
+      </div>
+</template>
+
+<script setup lang="ts">
+import { NigerianFlag, Edit } from '@/assets/icons';
+import PrimaryBtn from '@/components/Buttons/PrimaryBtn.vue';
+import SecondaryBtn from '@/components/Buttons/SecondaryBtn.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/mms-Admin/src/components/Mentors/MentorCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card">
+    <div class="flex gap-3 items-center">
+      <UserAvatar />
+      <div>
+        <h1 class="text-md font-semibold">Alison Davis</h1>
+        <p class="text-xs text-[#4D4D4D]">Added 0ct. 10 2022</p>
+      </div>
+    </div>
+    <div class="flex flex-col items-center justify-center">
+      <SmallPrimaryBtn title="View" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import UserAvatar from "../Common/UserAvatar.vue";
+import SmallPrimaryBtn from "../Buttons/SmallPrimaryBtn.vue";
+</script>
+
+<style scoped lang="scss">
+.card {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 15px;
+  background-color: white;
+  border-radius: 7px;
+  border: 1px solid var(--border);
+  width: 100%;
+  margin-bottom: 10px;
+
+  &:hover {
+    background-color: var(--light-grid-background);
+  }
+
+  &.active {
+    background-color: var(--light-grid-background);
+    border: none !important;
+  }
+}
+</style>
