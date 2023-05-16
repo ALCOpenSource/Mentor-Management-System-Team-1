fae87ac0c9ed77ecc92b62320b1ce785fac065c6
Idoko Emmanuel <idokoemmanuel3@gmail.com>
2023-05-16 01:53:37 +0100

    integrated threads and initial conversations

---
+++ frontend/mms-Admin/src/store/message.ts
@@ -0,0 +1,35 @@
+import {defineStore} from 'pinia'
+import axios from "axios"
+
+interface MessageState {
+    threads: object | null;
+    thread: object | null;
+}
+
+export const useMessageStore = defineStore({
+    id: 'message',
+
+    state: (): MessageState => {
+        return {
+            threads: null,
+            thread: null,
+        }
+    },
+
+    getters:{
+        getThreads: (state) => state.threads,
+        getThread: (state) => state.thread,
+    },
+
+    actions: {
+        async loadThreads() {
+            const res = await axios.get('v1/message/threads')
+            this.threads = res.data
+        },
+
+        async loadThread(roomid: string) {
+            const res = await axios.get('v1/message/thread/' + roomid)
+            this.thread = res.data
+        },
+    }
+})
