cc6f6e210e61439f12b0ac35237f472ec1d8eb1a
Idoko Emmanuel <idokoemmanuel3@gmail.com>
2023-05-26 13:15:54 +0100

    updated pagination logic

--- frontend/mms-Admin/src/components/Common/Pagination.vue
+++ frontend/mms-Admin/src/components/Common/Pagination.vue
@@ -2,7 +2,7 @@
   <div class="flex w-[165px] justify-between items-center">
     <ArrowLeft2 @click="$emit('fetchPage', 'first')" class="cursor-pointer" />
     <ArrowLeft @click="$emit('fetchPage', 'previous')" class="cursor-pointer" />
-    <p class="font-semibold whitespace-nowrap">{{ (pagination.current_page * pagination.count) - (pagination.count - 1) }} - {{ pagination.current_page * pagination.count }} of {{ pagination.total_pages }}</p>
+    <p class="font-semibold whitespace-nowrap">{{ (pagination.current_page * pagination.per_page) - (pagination.per_page - 1) }} - {{ pagination.current_page * pagination.per_page }} of {{ pagination.total }}</p>
     <ArrowRight @click="$emit('fetchPage', 'next')" class="cursor-pointer" />
     <ArrowRight2 @click="$emit('fetchPage', 'last')" class="cursor-pointer" />
   </div>
